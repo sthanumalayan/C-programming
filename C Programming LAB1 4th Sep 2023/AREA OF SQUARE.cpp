5e616015f92def4ec06da098f423100850b7fb0b
sthanumalayan <143411523+sthanumalayan@users.noreply.github.com>
2023-09-04 16:32:26 +0530

    Add files via upload
    4th Sep 2023 LAB 1 C programming

---
+++ C Programming LAB1 4th Sep 2023/AREA OF SQUARE.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+	int r;
+	scanf("%d",&r);
+	int area= r*r;
+	printf("THE AREA IS %d",area);
+	return 0;
+}
---
+++ C Programming LAB1 4th Sep 2023/CONVERTING KM TO CM.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int  main(){
+	int km;
+	scanf("%d",&km);
+	int cm=km*1000;
+	printf("%d in cm is %d",km,cm);
+}
