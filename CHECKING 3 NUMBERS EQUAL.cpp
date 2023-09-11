ad3086607d5d392d698aac9f84e76c250a01c7b1
sthanumalayan <143411523+sthanumalayan@users.noreply.github.com>
2023-09-11 19:11:54 +0530

    Delete CHECKING 3 NUMBERS EQUAL.cpp

--- CHECKING 3 NUMBERS EQUAL.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-	int a,b,c;
-	printf("ENTER VALUES OF A,B,C");
-	scanf("%d %d %d",&a,&b,&c);
-	if((a==b)&&(b==c)&&(c==a))printf("ALL ARE EQUAL");
-	else printf("ALL ARE NOT EQUAL");
-	return 0;
-}
